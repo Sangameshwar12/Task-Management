a3d9dc267eeb93913932a474f62dfad6a75cc003
Sangamesh <sangamchawale726@gmail.com>
2024-03-17 11:03:51 +0530

    Task Management Project started

---
+++ src/main/java/com/sangam/Task/Management/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.sangam.Task.Management.repository;
+
+import com.sangam.Task.Management.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
